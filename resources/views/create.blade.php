b8809da23362799ac355b72cf58158bc91328fb7
yasimaru00 <yasimaru00@gmail.com>
2022-12-27 22:12:23 +0700

    fix change ui

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,54 +1,48 @@
 <!doctype html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel CRUD With Multiple Image Upload</title>
 
-      <!-- Bootstrap CSS -->
-     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-     <!-- Font-awesome -->
-     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel Image Manga And Background Manga </title>
 
-    </head>
-    <body>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <!-- Font-awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 
-        <div class="container" style="margin-top: 50px;">
-            <div class="row">
+</head>
+
+<body>
+
+    <div class="container" style="margin-top: 50px;">
+        <div class="row">
 
 
-                <div class="col-lg-3"></div>
-                <div class="col-lg-6">
-                    <h3 class="text-center text-danger"><b>Add New Post</b> </h3>
-				    <div class="form-group">
-                        <form action="/post" method="post" enctype="multipart/form-data">
-                         @csrf
-        				 <input type="text" name="title" class="form-control m-2" placeholder="title">
-        				 <input type="text" name="author" class="form-control m-2" placeholder="author">
-                         <Textarea name="body" cols="20" rows="4" class="form-control m-2" placeholder="body"></Textarea>
-                         <label class="m-2">Cover Image</label>
-                         <input type="file" id="input-file-now-custom-3" class="form-control m-2" name="cover">
+            <div class="col-lg-3"></div>
+            <div class="col-lg-6">
+                <h3 class="text-center text-success"><b>Tambah Data Manga</b> </h3>
+                <div class="form-group">
+                    <form action="/post" method="post" enctype="multipart/form-data">
+                        @csrf
+                        <input type="text" name="title" class="form-control m-2" placeholder="Judul Manga">
+                        <input type="text" name="author" class="form-control m-2" placeholder="Pembuat Manga">
+                        <Textarea name="body" cols="20" rows="4" class="form-control m-2" placeholder="Keterangan Manga"></Textarea>
+                        <label class="m-2">Sampul Image Manga</label>
+                        <input type="file" id="input-file-now-custom-3" class="form-control m-2" name="cover">
 
-                         <label class="m-2">Images</label>
-                         <input type="file" id="input-file-now-custom-3" class="form-control m-2" name="images[]" multiple>
+                        <label class="m-2">Images Manga</label>
+                        <input type="file" id="input-file-now-custom-3" class="form-control m-2" name="images[]" multiple>
 
-                        <button type="submit" class="btn btn-danger mt-3 ">Submit</button>
-                        </form>
-                   </div>
+                        <button type="submit" class="btn btn-primary mt-3 ">Submit</button>
+                    </form>
                 </div>
             </div>
+        </div>
 
 
 
-         </body>
+</body>
+
 </html>
-
-
-
-
-
-
-
-
-
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,64 +1,78 @@
 <!doctype html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel CRUD With Multiple Image Upload</title>
 
-      <!-- Bootstrap CSS -->
-     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-     <!-- Font-awesome -->
-     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Laravel Image Manga And Background Manga </title>
 
-    </head>
-    <body>
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+  <!-- Font-awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 
-        <div class="container" style="margin-top: 50px;">
+</head>
 
-            <h3 class="text-center text-danger"><b>Laravel CRUD With Multiple Image Upload</b> </h3>
-            <a href="/create" class="btn btn-outline-success">Add New Post</a>
+<body>
 
-            <table class="table">
-                <thead>
-                  <tr>
-                    <th>ID</th>
-                    <th>Title</th>
-                    <th>Author</th>
-                    <th>Description</th>
-                    <th>Cover</th>
-                    <th>Update</th>
-                    <th>Delete</th>
-                  </tr>
-                </thead>
-                <tbody>
+  <div class="container" style="margin-top: 50px;">
 
+    <h3 class="text-center text-success"><b>Laravel Image Manga And Background Manga </b> </h3> <br><br>
+    <a href="/create" class="btn btn-outline-success">Tambah Data Manga</a> <br><br>
 
-                    @foreach ($posts as $post)
-                 <tr>
-                       <th scope="row">{{ $post->id }}</th>
-                       <td>{{ $post->title }}</td>
-                       <td>{{ $post->author }}</td>
-                       <td>{{ $post->body }}</td>
-                       <td><img src="cover/{{ $post->cover }}" class="img-responsive" style="max-height:100px; max-width:100px" alt="" srcset=""></td>
-                       <td><a href="/edit/{{ $post->id }}" class="btn btn-outline-primary">Update</a></td>
-                       <td>
-                           <form action="/delete/{{ $post->id }}" method="post">
-                            <button class="btn btn-outline-danger" onclick="return confirm('Are you sure?');" type="submit">Delete</button>
-                            @csrf
-                            @method('delete')
-                        </form>
-                       </td>
-
-                   </tr>
-                   @endforeach
-
-                </tbody>
-              </table>
-        </div>
+    <table class="table table-dark">
+      <thead>
+        <tr>
+          <th>No</th>
+          <th>Judul Manga</th>
+          <th>Pembuat Manga</th>
+          <th>Keterangan Manga</th>
+          <th>Sampul Manga</th>
+          <th>Perbaharui</th>
+          <th>Hapus</th>
+        </tr>
+      </thead>
+      <tbody>
 
 
+        @foreach ($posts as $post)
+        <tr>
+          <th scope="row">{{ $post->id }}</th>
+          <td>{{ $post->title }}</td>
+          <td>{{ $post->author }}</td>
+          <td>{{ $post->body }}</td>
+          <td><img src="cover/{{ $post->cover }}" class="img-responsive" style="max-height:100px; max-width:100px" alt="" srcset=""></td>
+          <td><a href="/edit/{{ $post->id }}" class="btn btn-outline-primary">Perbaharui</a></td>
+          <td>
+            <form action="/delete/{{ $post->id }}" method="post">
+              <button class="btn btn-outline-danger" onclick="return confirm('Are you sure?');" type="submit">Hapus</button>
+              @csrf
+              @method('delete')
+            </form>
+          </td>
+
+        </tr>
+        @endforeach
+
+      </tbody>
+    </table>
+  </div>
+
+  <div class="container">
+    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+      <div class="col-md-4 d-flex align-items-center">
+        <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+          <svg class="bi" width="30" height="24">
+            <use xlink:href="#bootstrap"></use>
+          </svg>
+        </a>
+        <span class="mb-3 mb-md-0 text-muted">© Galiley Singgang M.Y TI-3G </span>
+      </div>
+    </footer>
+  </div>
 
 
-    </body>
+</body>
+
 </html>
